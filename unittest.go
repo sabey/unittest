37974af558022c115d90eb464eda1357090b3bec
Jackson Sabey <jackson@juandefu.ca>
2016-07-31 02:42:59 -0700

    renamed namespace moved namespace above copyright

--- unittest.go
+++ unittest.go
@@ -1,12 +1,13 @@
+package unittest
+
 // Copyright 2015, JuanDeFu.ca. All rights reserved.
 // Use of this source code is governed by a BSD-style license that can be found in the LICENSE file.
-package unittest
 
 import (
 	"fmt"
-	"github.com/sabey/stacktrace"
 	"log"
 	"reflect"
+	"sabey.co/stacktrace"
 	"testing"
 )
 
